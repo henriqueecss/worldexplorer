83fda46b55aa2d6f786e9cf3029a2f3de58b0214
Henrique Cesar <henrique.soares1@estudante.ufla.br>
2026-05-11 16:17:45 -0300

    feat: wire up all providers, contexts and routes in App
    Wraps the app with ThemeProvider, ToastProvider and FavoritesProvider.
    Adds routes for /register, /forgot-password and /country/:cca3 alongside
    the existing protected routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,47 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import Register from './pages/Register'
+import ForgotPassword from './pages/ForgotPassword'
 import PrivateRoute from './components/PrivateRoute'
 import Favorites from './pages/Favorites'
+import CountryDetail from './pages/CountryDetail'
+import { FavoritesProvider } from './context/FavoritesContext'
+import { ToastProvider } from './context/ToastContext'
+import { ThemeProvider } from './context/ThemeContext'
+import Toast from './components/Toast'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={
-          <PrivateRoute>
-            <Home />
-          </PrivateRoute>
-        } />
-        <Route path="/favorites" element={
-          <PrivateRoute>
-            <Favorites />
-          </PrivateRoute>
-        } />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider>
+    <ToastProvider>
+      <FavoritesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/" element={
+              <PrivateRoute>
+                <Home />
+              </PrivateRoute>
+            } />
+            <Route path="/favorites" element={
+              <PrivateRoute>
+                <Favorites />
+              </PrivateRoute>
+            } />
+            <Route path="/country/:cca3" element={
+              <PrivateRoute>
+                <CountryDetail />
+              </PrivateRoute>
+            } />
+          </Routes>
+        </BrowserRouter>
+        <Toast />
+      </FavoritesProvider>
+    </ToastProvider>
+    </ThemeProvider>
   )
 }
 
