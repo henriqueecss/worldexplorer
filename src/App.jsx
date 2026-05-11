31182115ece74c51a7d0cb071b682779760c91b4
Henrique Cesar <henrique.soares1@estudante.ufla.br>
2026-05-11 17:49:41 -0300

    fix: add wildcard route redirecting unknown paths to home

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
@@ -36,6 +36,7 @@ function App() {
                 <CountryDetail />
               </PrivateRoute>
             } />
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </BrowserRouter>
         <Toast />
